6049294d3d11b9680bfe79835dc197bc55415c49
Quintin <quint.oliver@gmail.com>
2014-10-31 14:47:46 -0400

    finished description

--- Lab9/src/lab9/VehicleArrays.java
+++ Lab9/src/lab9/VehicleArrays.java
@@ -1,5 +1,5 @@
 /**
-* This is the Java program for Lab 9 of CS1180. The purpose of thos lab is to display 
+* This is the Java program for Lab 9 of CS1180. The purpose of thos lab is to display use of arrays and AreayList. The project will create an array amd an ArrayList of MotorVehicle objects amd display them.
 */
 package lab9;
 
@@ -13,4 +13,8 @@ public class VehicleArrays {
     public static void main(String[] args) {
         
     }//end main
+    
+	static void showDescription(){
+	
+	}
 }//end class
