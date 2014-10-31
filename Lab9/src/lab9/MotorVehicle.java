20881c1abc9e9371f78816ee0877344547151359
Quintin <quint.oliver@gmail.com>
2014-10-31 13:33:49 -0400

    new methods

--- Lab9/src/lab9/MotorVehicle.java
+++ Lab9/src/lab9/MotorVehicle.java
@@ -3,61 +3,59 @@ package lab9;
 import java.util.*;
 
 public class MotorVehicle{
-  private String vehicleType;
-  private int numberWheels;
-  private double engineSize;
-  private boolean powerSteering;
-  private GregorianCalendar purchaseDate;
+	private String vehicleType;
+	private int numberWheels;
+	private double engineSize;
+	private boolean powerSteering;
+	private GregorianCalendar purchaseDate;
   
-  public MotorVehicle() {
-    vehicleType = "Coupe";
-    numberWheels = 4;
-    engineSize = 1.6;
-    powerSteering = true;
-    purchaseDate = new GregorianCalendar();
-  }
+	public MotorVehicle() {
+		this("Coupe", 4, 1.6, true, new GregorianCalendar());
+	}
   
-  public MotorVehicle(String vehicleType) {
-    this.vehicleType = vehicleType;
-    numberWheels = 4;
-    engineSize = 1.6;
-    powerSteering = true;
-    purchaseDate = new GregorianCalendar();
-  }
+	public MotorVehicle(String vehicleType) {
+		this(vehicleType, 4, 1.6, true, new GregorianCalendar());
+	}
   
-  public MotorVehicle(String vehicleType, int numberWheels, double engineSize, boolean powerSteering, GregorianCalendar purchaseDate){
-    this.vehicleType = vehicleType;
-    this.numberWheels = numberWheels;
-    this.engineSize = engineSize;
-    this.powerSteering = powerSteering;
-    this.purchaseDate = purchaseDate;
-  }
+	public MotorVehicle(String vehicleType, int numberWheels, double engineSize, boolean powerSteering, GregorianCalendar purchaseDate){
+		this.vehicleType = vehicleType;
+		this.numberWheels = numberWheels;
+		this.engineSize = engineSize;
+		this.powerSteering = powerSteering;
+		this.purchaseDate = purchaseDate;
+	}
   
-  public void setVehicleType(String vehicleType) {
-    this.vehicleType = vehicleType;
-  }
+	public void setVehicleType(String vehicleType) {
+		this.vehicleType = vehicleType;
+	}
   
-  public String getVehicleType() {
-    return vehicleType;
-  }
+	public String getVehicleType() {
+		return vehicleType;
+	}
   
-  public void setNumberWheels(int numberWheels) {
-    this.numberWheels = numberWheels;
-  }
+	public void setNumberWheels(int numberWheels) {
+		this.numberWheels = numberWheels;
+	}
   
-  public int getNumberWheeld() {
-    return numberWheels;
-  }
+	public int getNumberWheeld() {
+		return numberWheels;
+	}
   
-  public void setEngineSize(double engineSize) {
-    this.engineSize = engineSize;
-  }
+	public void setEngineSize(double engineSize) {
+		this.engineSize = engineSize;
+	}
   
-  public double getEngineSize() {
-    return engineSize;
-  }
+	public double getEngineSize() {
+		return engineSize;
+	}
   
-  public void setPowerSteering(boolean powerSteering){
-    this.powerSteering = powerSteering;
-  }
- }
+	public void setPowerSteering(boolean powerSteering){
+		this.powerSteering = powerSteering;
+	}
+  
+	public boolean getPowerSteering(){
+		return powerSteering;
+	}
+	
+	public void setPurchaseDate(GregorianCalendar )
+}
