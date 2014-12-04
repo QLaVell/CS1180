43fdfac992a3c063050df0b19a2f6441ce390397
Quintin <quint.oliver@gmail.com>
2014-12-04 18:46:04 -0500

    Config class containing constants

---
+++ FinalGame/src/finalgame/Config.java
@@ -0,0 +1,17 @@
+
+/**
+ * [describe class here]
+ */
+
+package finalgame;
+
+/**
+* Quintin Oliver
+* Section 90
+* TA: Katrina Scott
+* Michael Ondrasek 
+*/
+public class Config {
+    public static final int WINDOW_WIDTH = 630;
+    public static final int WINDOW_HEIGHT = 350;
+}
